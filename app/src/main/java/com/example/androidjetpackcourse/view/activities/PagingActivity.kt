376b9945e79e242727c1d9db778822b5b2f1a1d8
royhayek <royhayek27@gmail.com>
2021-09-16 13:11:41 +0300

    Used LiveData instead of Flow

--- app/src/main/java/com/example/androidjetpackcourse/view/activities/PagingActivity.kt
+++ app/src/main/java/com/example/androidjetpackcourse/view/activities/PagingActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.cachedIn
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidjetpackcourse.R
 import com.example.androidjetpackcourse.databinding.ActivityPagingBinding
@@ -17,7 +18,7 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class PagingActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPagingBinding
+    private lateinit var binding : ActivityPagingBinding
     lateinit var recyclerViewAdapter: GitRepoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,19 +40,18 @@ class PagingActivity : AppCompatActivity() {
     }
 
     private fun initViewModel() {
-        val viewModel = ViewModelProvider(this).get(GitRepoViewModel::class.java)
+        val viewModel  = ViewModelProvider(this).get(GitRepoViewModel::class.java)
 
-        // here we are sticking this coroutine to the lifecycle of this activity
+        // sticking this coroutine to the lifecycle of this activity
         // if the activity is destroyed  everything launched inside this scope will be destroyed
         // here we didn't use the GlobalScope to prevent memory leaks because when this activity
         // will be destroyed the resources will stay in the GlobalScope
         lifecycleScope.launch {
             // Here we are observing the PageData stream
-            // since the ViewModel returns a Flow, we can use collectLast to access the stream
-            viewModel.getListData().collectLatest { pagingData ->
-                // once we have the results we send the list to the adapter by calling submitData
-                recyclerViewAdapter.submitData(pagingData)
-            }
+            // once we have the results we send the list to the adapter by calling submitData
+            viewModel.getListData().observe(this@PagingActivity, {
+                recyclerViewAdapter.submitData(lifecycle, it)
+            })
         }
     }
 }
--- app/src/main/java/com/example/androidjetpackcourse/viewmodel/GitRepoViewModel.kt
+++ app/src/main/java/com/example/androidjetpackcourse/viewmodel/GitRepoViewModel.kt
@@ -1,26 +1,24 @@
 package com.example.androidjetpackcourse.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
+import androidx.paging.*
 import com.example.androidjetpackcourse.data.model.GitRepo
+import com.example.androidjetpackcourse.data.model.GitRepoResponse
 import com.example.androidjetpackcourse.data.network.GitRepoPagingSource
 import com.example.androidjetpackcourse.data.network.GitRepoService
 import com.example.androidjetpackcourse.data.network.GitRepoServiceBuilder
 import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
 
-class GitRepoViewModel : ViewModel() {
+class GitRepoViewModel() : ViewModel() {
 
     private lateinit var service: GitRepoService
 
     // Fetching repos from the PagingSource
-    // A Flow emit multiple values sequentially
-    // Opposed to the suspend functions which returns a single value
-    fun getListData(): Flow<PagingData<GitRepo>> {
+    fun getListData(): LiveData<PagingData<GitRepo>> {
         // Here we created a reference to the GitRepoService to download the repos list
         service = GitRepoServiceBuilder.buildService(GitRepoService::class.java)
 
@@ -28,15 +26,18 @@ class GitRepoViewModel : ViewModel() {
         // which is used to fetch a stream of data from PagingSource
         // the Paging library supports several return types
         // Flow, LiveData, and RxJava(Flowable, Observable)
-        // Here we used Flow from coroutines
+        // Here we used LiveData
         return Pager(
             // Configuring how data is loaded by adding additional properties to PagingConfig
-            config = PagingConfig(pageSize = 20, enablePlaceholders = false)
-        ) {
-            // Here we are calling the load function of the paging source which is returning a LoadResult
-            GitRepoPagingSource(service)
-        }.flow.cachedIn(viewModelScope)
-        // cachedIn makes the data stream shareable and caches the loaded data with the provided
-        // CoroutineScope, here we are using the viewModelScope
+            config = PagingConfig(
+                pageSize = 20,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                // Here we are calling the load function of the paging source which is returning a LoadResult
+                GitRepoPagingSource(service)
+            }
+        ).liveData
+            .cachedIn(viewModelScope)
     }
 }
