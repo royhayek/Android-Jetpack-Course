f5dca5d9885d80ed193666d74a61d84c4903008f
royhayek <royhayek27@gmail.com>
2021-09-27 17:47:28 +0300

    Design weather details activity

---
+++ app/src/main/java/com/example/androidjetpackcourse/view/activities/WeatherDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidjetpackcourse.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidjetpackcourse.R
+
+class WeatherDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_details)
+
+    }
+}
